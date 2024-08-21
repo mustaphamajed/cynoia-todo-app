9d8e7065414595fe4974ff34e43424d4b0e93d33
mustaphamajed <mustapha.majed@esprit.tn>
2024-08-21 14:18:06 +0100

    style(colors):add colors

---
+++ src/helpers/colors.ts
@@ -0,0 +1,30 @@
+
+
+export const Colors = {
+ 
+
+    white: '#FFFFFF',
+    black: '#000000',
+    dark: '#020021',
+    red: '#FF0000',
+    orange: '#FFA500',
+    lightRed: '#F75555',
+    darkBlue: '#000088',
+    arcticLime: '#C3FA15',
+    gray98: '#FAFAFA',
+    darkGray98: '#1F222A',
+    grayFA: '#FAFAFA',
+    gray62: '#9E9E9E',
+    gray42: '#F3F4F6',
+    grayEE: '#EEEEEE',
+    grayEO: '#E0E0E0',
+    darkText: '#212121',
+    grayText: '#616161',
+    fcbColor: '#4267B2',
+    lightGray: '#F7F8F9',
+    lightGreen: '#12D18E',
+    blue: '#2F80ED',
+    successColor: '#17CE92',
+    navyBlue: '#0a065e70',
+    purple:'#8d45ba'
+};
