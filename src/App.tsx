2eb4652fab8cb30fc23983e8808bf58d81ede7ed
mustaphamajed <mustapha.majed@esprit.tn>
2024-08-22 12:45:04 +0100

    refactor(app): integrate refactor app

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,27 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import AppNavigator from './navigation/appNavigator';
 import {NavigationContainer} from '@react-navigation/native';
-
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {QueryClientProvider} from '@tanstack/react-query';
+import queryClient from './queryClient';
+import {Provider} from 'react-redux';
+import {store} from './redux/store';
 const App = () => {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <GestureHandlerRootView style={styles.container}>
+          <AppNavigator />
+        </GestureHandlerRootView>
+      </QueryClientProvider>
+    </Provider>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
