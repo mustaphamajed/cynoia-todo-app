537a7a850c78a8b1e792c3b24306e4071aa7139b
mustaphamajed <mustapha.majed@esprit.tn>
2024-08-22 13:01:13 +0100

    feat(home-screen): add navigation to new project screen

--- src/screens/main/Home/homeScreen.tsx
+++ src/screens/main/Home/homeScreen.tsx
@@ -3,29 +3,44 @@ import React from 'react';
 import {HomeHeader} from '../../../components/headers';
 import ProjectCard from '../../../components/cards/projectCard';
 import commonStyles from '../../../helpers/commonStyles';
-import {
-  CustomButton,
-  CustomText,
-  ScreenContainer,
-} from '../../../components/shared';
+import {CustomText, ScreenContainer} from '../../../components/shared';
 import {staticProjectData} from '../../../helpers/static';
 import {Colors} from '../../../helpers/colors';
 import {useNavigation} from '@react-navigation/native';
 import {NavigationRoot} from '../../../interfaces';
 import {ROUTE_NAMES} from '../../../helpers/routes';
+import {useSelector} from 'react-redux';
+import {RootState} from '../../../redux/store';
+import {useQuery} from '@tanstack/react-query';
+import {fetchProjects} from '../../../service/projectService';
+import Octicons from 'react-native-vector-icons/Octicons';
 
 const HomeScreen = () => {
   const navigation = useNavigation<NavigationRoot>();
+  const {projects} = useSelector((state: RootState) => state.projects);
+
+  const {data, isLoading} = useQuery({
+    queryKey: ['projects'],
+    queryFn: fetchProjects,
+  });
   return (
     <ScreenContainer>
       <HomeHeader />
       <View style={commonStyles.px20}>
-        <CustomText
-          fontFamily={commonStyles.bold}
-          fontSize={24}
-          color={Colors.white}>
-          Projects
-        </CustomText>
+        <View style={[commonStyles.row, commonStyles.justifyBetween]}>
+          <CustomText
+            fontFamily={commonStyles.bold}
+            fontSize={22}
+            color={Colors.white}>
+            Projects
+          </CustomText>
+          <Octicons
+            name="plus"
+            size={28}
+            color={Colors.white}
+            onPress={() => navigation.navigate(ROUTE_NAMES.MAIN.NEW_PROJECT)}
+          />
+        </View>
         <View style={commonStyles.pt20}>
           {staticProjectData.map(({name, id, description}) => {
             return (
