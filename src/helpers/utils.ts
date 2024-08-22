a5bb2f9b03dbe65c14da5ad42df9ca9ac4afa428
mustaphamajed <mustapha.majed@esprit.tn>
2024-08-22 12:42:16 +0100

    feat(utils): add utils file

---
+++ src/helpers/utils.ts
@@ -0,0 +1,8 @@
+export const isValidPassword = (password: string): boolean => {
+    const passwordRegex = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}/;
+    return passwordRegex.test(password);
+};
+  
+export enum STORAGE {
+    accessToken = 'accessToken',
+  }
