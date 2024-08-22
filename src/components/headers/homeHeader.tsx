4bb81cb66622659a81a89a8da0d3d14b1207d09d
mustaphamajed <mustapha.majed@esprit.tn>
2024-08-23 00:18:45 +0100

    fix(header): fix home header

--- src/components/headers/homeHeader.tsx
+++ src/components/headers/homeHeader.tsx
@@ -1,29 +1,26 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import commonStyles from '../../helpers/commonStyles';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import {CustomText} from '../shared';
-import LinearGradient from 'react-native-linear-gradient';
+import {Colors} from '../../helpers/colors';
 
 const HomeHeader = () => {
   return (
     <View
       style={[commonStyles.row, commonStyles.p20, commonStyles.alignCenter]}>
-      <View style={[commonStyles.w40, commonStyles.justifyBetween]}>
+      <View
+        style={[
+          commonStyles.w40,
+          commonStyles.pt40,
+          commonStyles.justifyBetween,
+        ]}>
         <Text
           style={[
             commonStyles.fs14,
             commonStyles.medium,
-            {color: Colors.grayText},
+            {color: Colors.lightGray},
           ]}>
           Welcome!
         </Text>
-        <CustomText
-          fontSize={16}
-          fontFamily={commonStyles.bold}
-          extraStyle={{paddingVertical: 5}}>
-          username
-        </CustomText>
       </View>
     </View>
   );
