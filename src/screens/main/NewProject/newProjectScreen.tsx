cbedd6045ee86aec596701381575a57fd7b41694
mustaphamajed <mustapha.majed@esprit.tn>
2024-08-22 12:59:05 +0100

    feat(screens): create new project screen

---
+++ src/screens/main/NewProject/newProjectScreen.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const NewProjectScreen = () => {
+  return (
+    <View>
+      <Text>NewProjectScreen</Text>
+    </View>
+  );
+};
+
+export default NewProjectScreen;
+
+const styles = StyleSheet.create({});
--- src/screens/main/index.ts
+++ src/screens/main/index.ts
@@ -1,6 +1,8 @@
 import HomeScreen from "./Home/homeScreen";
 import ProjectDetails from "./ProjectDetails/projectDetails";
+import NewProjectScreen from "./NewProject/newProjectScreen";
 export {
     HomeScreen,
-    ProjectDetails
+    ProjectDetails,
+    NewProjectScreen
 }
